172a15bccbdf6a610b9d69b77415ae6a59bf3cdd
vivekd9939 <81351573+vivekd9939@users.noreply.github.com>
2021-04-07 14:48:49 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -4,7 +4,7 @@ echo " pass three arguments "
 elif [ $1 -gt $2 ] && [ $1 -gt $3 ]; then
 echo " $1 is big "
 elif [ $2 -gt $1 ] && [ $2 -gt $3 ]; then
-echo " $2 is big "
+echo " $2 is big number"
 else
 echo " $3 is big "
 fi
